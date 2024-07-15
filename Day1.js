03d3fd67d5d1334cdf2f66b013a539200291ffa9
Sanchit Ahuja <91591902+ahuja-sanchitt@users.noreply.github.com>
2024-07-15 21:20:33 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,46 @@
+//Task1
+
+// var num=5
+// console.log(num)
+
+// let f="sanchit"
+// console.log(f)
+
+
+//Task2
+
+// const check=true
+// console.log(check)
+
+
+//Task3
+
+// let a=5
+// let b="Sanchit"
+// let c= true
+// let d=["A", "B", "C"]
+// const e={"Name":"Sanchit","Age":23}
+
+// console.log(a)
+// console.log(typeof(a))
+// console.log(b)
+// console.log(typeof(b))
+// console.log(c)
+// console.log(typeof(c))
+// console.log(d)
+// console.log(typeof(d))
+// console.log(e)
+// console.log(typeof(e))
+
+//Task4
+
+// let f="Sanchit"
+// console.log(f)
+// f="Rahul"
+// console.log(f)
+
+//Task5
+const f="Sanchit"
+console.log(f)
+f="Rahul"
+console.log(f)
---
+++ Day2.js
@@ -0,0 +1,81 @@
+//ACTIVITY1
+
+// function Add(){
+//     var a=10;
+//     var b=20;
+//     console.log(a+b)
+// }
+
+// function Subtract(){
+//     var a=10;
+//     var b=20;
+//     console.log(a-b)
+// }
+// function Multiply(){
+//     var a=10;
+//     var b=20;
+//     console.log(a*b)
+// }
+// function Divide(){
+//     var a=10;
+//     var b=20;
+//     console.log(a/b)
+// }
+// function Remainder(){
+//     var a=10;
+//     var b=20;
+//     console.log(a%b)
+// }
+
+// Add();
+// Subtract();
+// Multiply();
+// Divide();
+// Remainder();
+
+//ACTIVITY2 
+
+// function Add2(){
+//     var a=10;
+//     a+=20;
+//     console.log(a)
+// }
+
+// function Subtract2(){
+//     var c=20;
+//     c-=20;
+//     console.log(c)
+// }
+
+// Add2();
+// Subtract2();
+
+//ACTIVITY3
+
+// var a=10
+// var b=20
+// var c=20
+// var d=20
+// console.log(a>b)
+// console.log(a<b)
+// console.log(c>=d)
+// console.log(c<=a)
+// console.log(2=='2')
+// console.log(2==='2')
+
+//ACTIVITY4
+
+// var a=10
+// var b=20
+// if(a<b && typeof(a)=="number") console.log("YAY") 
+//     else console.log("NAY!")
+// if(a>b || typeof(a)=="number") console.log("YAY")
+//     else console.log("NAY!")
+// if(a!=10) console.log("YAY")
+//     else console.log("NAY!")
+
+
+//ACTIVITY5
+
+// var num=5
+// console.log(num>-0?"POSITIVE":"NEGATIVE")
